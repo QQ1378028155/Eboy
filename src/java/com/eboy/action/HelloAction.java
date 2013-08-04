6776ed8ccff161a1213f0fce1d8e6a6e767b11d3
wjl <wjl.thunder@gmail.com>
2013-08-04 17:39:16 +0800

    Add Tag Support

--- src/java/com/eboy/action/HelloAction.java
+++ src/java/com/eboy/action/HelloAction.java
@@ -4,16 +4,44 @@
  */
 package com.eboy.action;
 
+import com.eboy.po.Tag;
+import com.eboy.service.TagService;
+import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
+import java.util.List;
 
 /**
  *
  * @author wjl
  */
 public class HelloAction extends ActionSupport{
+        
+        private TagService tagService;
+        private String tagWord;
         @Override
         public String execute()
         {
+                tagService.addTag(getTagWord());
+                List<Tag> tags = tagService.getTags();
+                ActionContext context = ActionContext.getContext();
+                context.put("tagWordRequest", tagWord);
+                context.put("tags", tags);
                 return "success";
+        }
+
+        public TagService getTagService() {
+                return tagService;
+        }
+
+        public void setTagService(TagService tagService) {
+                this.tagService = tagService;
+        }
+
+        public String getTagWord() {
+                return tagWord;
+        }
+
+        public void setTagWord(String tagWord) {
+                this.tagWord = tagWord;
         }
 }
--- web/jsp/hello.jsp
+++ web/jsp/hello.jsp
@@ -3,7 +3,7 @@
     Created on : 2013-8-2, 18:40:17
     Author     : wjl
 --%>
-
+<%@taglib prefix="s" uri="/struts-tags" %>
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
@@ -13,5 +13,10 @@
         </head>
         <body>
                 <h1>Hello!</h1>
+                <s:property value="tagWord"/>
+                <s:property value="tagWordRequest"/>
+                <s:iterator value="#tags">
+                        <p><s:property value="tagWord"/></p>
+                </s:iterator>
         </body>
 </html>
