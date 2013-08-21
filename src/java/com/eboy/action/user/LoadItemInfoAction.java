5df87666a39a76021431c21499c25d4f40341557
game3108 <game3108@192.168.1.5>
2013-08-21 14:03:24 +0800

    add CommentService to loadItemInfoAction

--- src/java/com/eboy/action/user/LoadItemInfoAction.java
+++ src/java/com/eboy/action/user/LoadItemInfoAction.java
@@ -4,10 +4,13 @@
  */
 package com.eboy.action.user;
 
+import com.eboy.po.Comment;
 import com.eboy.po.Item;
+import com.eboy.service.CommentService;
 import com.eboy.service.ItemService;
 import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
+import java.util.List;
 
 /**
  *
@@ -15,14 +18,25 @@ import com.opensymphony.xwork2.ActionSupport;
  */
 public class LoadItemInfoAction extends ActionSupport{
         private ItemService itemService;
+        private CommentService commentService;
         private Integer itemId;
         @Override
         public String execute()
         {
                 Item item = itemService.getItem(itemId);
+                List<Comment> comments = commentService.getCommentsByItemId(itemId);
                 ActionContext context = ActionContext.getContext();
                 context.put("item", item);
+                context.put("comments",comments);
                 return "success";
+        }
+
+        public CommentService getCommentService() {
+                return commentService;
+        }
+
+        public void setCommentService(CommentService commentService) {
+                this.commentService = commentService;
         }
 
         public ItemService getItemService() {
